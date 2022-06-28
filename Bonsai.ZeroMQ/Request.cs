b5f01105e4946afc5c9b46db61fed4f517bf16b0
RoboDoig <erskine94@gmail.com>
2022-06-28 10:55:04 +0100

    example response-request

---
+++ Bonsai.ZeroMQ/Request.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Linq;
+using Bonsai.Osc;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Bonsai.ZeroMQ
+{
+    public class Request : Combinator<Message, string>
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+
+        public override IObservable<string> Process(IObservable<Message> source)
+        {
+            return Observable.Using(() =>
+            {
+                var request = new RequestSocket($">tcp://{Host}:{Port}");
+                //request.Bind($">tcp://{Host}:{Port}");
+                return request;
+            },
+            request => source.Select(
+                message =>
+                {
+                    request.SendFrame(message.Buffer.Array);
+                    string messageReturn = request.ReceiveFrameString();
+
+                    return messageReturn;
+                }).Finally(() => { request.Dispose(); })
+            );;
+        }
+    }
+}
---
+++ Bonsai.ZeroMQ/Response.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Bonsai.Osc;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Bonsai.ZeroMQ
+{
+    public class Response : Combinator<Message, byte[]>
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string ResponseMessage { get; set; } // TODO - this should be an actual OSC message
+
+        public override IObservable<byte[]> Process(IObservable<Message> source)
+        {
+            return Observable.Using(() =>
+            {
+                var response = new ResponseSocket($"tcp://*:{Port}");
+                return response;
+            },
+            response => source.Select(
+                message =>
+                {
+                    var bytes = response.ReceiveFrameBytes();
+                    response.SendFrame(ResponseMessage);
+                    return bytes;
+                }).Finally(() => { response.Dispose(); })
+            ); ;
+        }
+    }
+}
