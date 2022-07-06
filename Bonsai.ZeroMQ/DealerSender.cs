3877952ab491b29b6dd94e8040edf923f5c960d0
RoboDoig <erskine94@gmail.com>
2022-07-06 17:02:25 +0100

    fix for router sender, eager send based on message incoming

--- Bonsai.ZeroMQ/DealerSender.cs
+++ Bonsai.ZeroMQ/DealerSender.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 using Bonsai.Osc;
 using NetMQ;
 using NetMQ.Sockets;
---
+++ Bonsai.ZeroMQ/RouterSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Bonsai.Osc;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Bonsai.ZeroMQ
+{
+    [Combinator]
+    public class RouterSender
+    {
+        public IObservable<Tuple<uint, Message>> Process(IObservable<RouterSocket> source, IObservable<Tuple<uint, Message>> clientMessage)
+        {
+            return clientMessage.Do(m =>
+            {
+                var router = source.TakeLast(1);
+                var messageToClient = new NetMQMessage();
+                messageToClient.Append(m.Item1);
+                messageToClient.AppendEmptyFrame();
+                messageToClient.Append(m.Item2.Buffer.Array);
+                router.Do(r =>
+                {
+                    r.SendMultipartMessage(messageToClient);
+                });
+            });
+        }
+    }
+}
