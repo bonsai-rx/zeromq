5c0a6659e2924873e6614c762d6357a873266b58
RoboDoig <erskine94@gmail.com>
2022-07-07 11:30:44 +0100

    initial structure

---
+++ Bonsai.ZeroMQ/DealerNameConverter.cs
@@ -0,0 +1,34 @@
+﻿using Bonsai.Expressions;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Bonsai.ZeroMQ
+{
+    public class DealerNameConverter : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            if (context != null)
+            {
+                var workflowBuilder = (WorkflowBuilder)context.GetService(typeof(WorkflowBuilder));
+                if (workflowBuilder != null)
+                {
+                    var dealerNames = (from builder in workflowBuilder.Workflow.Descendants()
+                                        let dealer = ExpressionBuilder.GetWorkflowElement(builder) as CreateDealer
+                                        where dealer != null && !string.IsNullOrEmpty(dealer.Name)
+                                        select dealer.Name)
+                    .Distinct()
+                    .ToList();
+                    return new StandardValuesCollection(dealerNames);
+                }
+            }
+
+            return base.GetStandardValues(context);
+        }
+    }
+}
--- Bonsai.ZeroMQ/DealerSender.cs
+++ Bonsai.ZeroMQ/DealerSender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Reactive.Linq;
 using Bonsai.Osc;
 using NetMQ;
@@ -6,24 +7,27 @@ using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
-    [Combinator]
-    public class DealerSender
+    public class DealerSender : Combinator<Message, Message>
     {
-        public IObservable<Message> Process(IObservable<DealerSocket> source, IObservable<Message> message)
+        //public IObservable<Message> Process(IObservable<DealerSocket> source, IObservable<Message> message)
+        //{
+        //    return source.SelectMany(dealer =>
+        //    {
+        //        return message.Do(m =>
+        //        {
+        //            dealer.SendMoreFrameEmpty().SendFrame(m.Buffer.Array);
+        //        });
+        //    });
+        //}
+
+        [TypeConverter(typeof(DealerNameConverter))]
+        public string DealerConnection { get; set; }
+
+        public override IObservable<Message> Process<Message>(IObservable<Message> message)
         {
-            //return message.Do(m =>
-            //{
-            //    source.Do(d =>
-            //    {
-            //        d.SendMoreFrameEmpty().SendFrame(m.Buffer.Array);
-            //    });
-            //});
-            return source.SelectMany(dealer =>
+            message.Do(m =>
             {
-                return message.Do(m =>
-                {
-                    dealer.SendMoreFrameEmpty().SendFrame(m.Buffer.Array);
-                });
+
             });
         }
     }
