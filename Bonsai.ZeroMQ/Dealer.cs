5e71f1db820361ee5de188ab2d007ecc03b94e99
RoboDoig <erskine94@gmail.com>
2022-07-08 16:20:33 +0100

    basic structure of dual use router and dealer nodes

--- Bonsai.ZeroMQ/Dealer.cs
+++ Bonsai.ZeroMQ/Dealer.cs
@@ -7,30 +7,64 @@ using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
-    public class Dealer : Combinator<Message, byte[]>
+    [Combinator]
+    public class Dealer
     {
         public string Host { get; set; }
         public string Port { get; set; }
 
-        public override IObservable<byte[]> Process(IObservable<Message> source)
+        // Act as listener with no message - TODO this should call overload with empty message to avoid repeated code
+        public IObservable<byte[]> Process()
         {
-            return Observable.Using(() =>
+            return Observable.Create<byte[]>((observer, cancellationToken) =>
             {
                 var dealer = new DealerSocket();
                 dealer.Connect($"tcp://{Host}:{Port}");
-                return dealer; // "client"
-            },
-            dealer => source.Select(
-                message =>
+
+                return Task.Factory.StartNew(() =>
                 {
-                    dealer.SendMoreFrameEmpty().SendFrame(message.Buffer.Array);
+                    while(!cancellationToken.IsCancellationRequested)
+                    {
+                        var messageFromServer = dealer.ReceiveMultipartMessage();
+                        observer.OnNext(messageFromServer[1].ToByteArray());
+                    }
+                });
+            });
+        }
 
-                    var serverMessage = dealer.ReceiveMultipartMessage();
+        // Acts as sender and listener
+        public IObservable<byte[]> Process(IObservable<Message> message)
+        {
+            // TODO - this needs to be disposed along with everything else
+            Observable.Using(() =>
+            {
+                var dealer = new DealerSocket();
+                dealer.Connect($"tcp://{Host}:{Port}");
+                return dealer;
+            },
+            dealer =>
+            {
+                return message.Do(m =>
+                {
+                    dealer.SendMoreFrameEmpty().SendFrame(m.Buffer.Array);
+                    Console.WriteLine("sending");
+                });
+            }).Subscribe();
 
-                    return serverMessage[1].ToByteArray();
+            return Observable.Create<byte[]>((observer, cancellationToken) =>
+            {
+                var dealer = new DealerSocket();
+                dealer.Connect($"tcp://{Host}:{Port}");
 
-                }).Finally(() => { dealer.Dispose(); })
-            ); ;
+                return Task.Factory.StartNew(() =>
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        var messageFromServer = dealer.ReceiveMultipartMessage();
+                        observer.OnNext(messageFromServer[1].ToByteArray());
+                    }
+                });
+            });
         }
     }
 }
--- Bonsai.ZeroMQ/Router.cs
+++ Bonsai.ZeroMQ/Router.cs
@@ -7,47 +7,81 @@ using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
-    public class Router : Combinator<Message, Router.IncomingMessage>
+    [Combinator]
+    public class Router
     {
         public string Host { get; set; }
         public string Port { get; set; }
 
-        public override IObservable<IncomingMessage> Process(IObservable<Message> source)
+        // Act only as client listener
+        public IObservable<ClientMessage> Process()
         {
-            return Observable.Using(() =>
+            return Observable.Create<ClientMessage>((observer, cancellationToken) =>
             {
                 var router = new RouterSocket();
                 router.Bind($"tcp://{Host}:{Port}");
-                return router; // "server"
-            },
-            router => source.Select(
-                message =>
+
+                return Task.Factory.StartNew(() =>
                 {
-                    var clientMessage = router.ReceiveMultipartMessage(); // This method doesn't really work. We only try and receive a message when our source emits. Actully we want to listen for clients and then send messages back to clients when we get a message. On the other hand, what would we send back if the server has nothing to emit?
-                    uint clientAddress = (uint)clientMessage[0].ConvertToInt32();
-                    var messagePayload = clientMessage[2].ToByteArray(); // Index as two as 2nd message entry is empty delimiter
+                    while(!cancellationToken.IsCancellationRequested)
+                    {
+                        var messageFromClient = router.ReceiveMultipartMessage();
+                        byte[] clientAddress = messageFromClient[0].ToByteArray();
+                        byte[] messagePayload = messageFromClient[2].ToByteArray();
 
-                    var messageToClient = new NetMQMessage();
-                    messageToClient.Append(clientMessage[0].ToByteArray());
-                    messageToClient.AppendEmptyFrame();
-                    messageToClient.Append(message.Buffer.Array);
-                    router.SendMultipartMessage(messageToClient);
-
-                    return new IncomingMessage(clientAddress, messagePayload);
-                })
-            );
+                        observer.OnNext(new ClientMessage { ClientAddress = clientAddress, MessagePayload = messagePayload });
+                    }
+                }).ContinueWith(task => { router.Dispose(); });
+            });
         }
 
-        public struct IncomingMessage
+        //public IObservable<byte[]> Process(IObservable<Message> message)
+        //{
+
+        //}
+
+        public struct ClientMessage
         {
-            public uint RoutingId;
+            public byte[] ClientAddress;
             public byte[] MessagePayload;
-
-            public IncomingMessage(uint routingId, byte[] messagePayload)
-            {
-                RoutingId = routingId;
-                MessagePayload = messagePayload;
-            }
         }
+
+        //public override IObservable<IncomingMessage> Process(IObservable<Message> source)
+        //{
+        //    return Observable.Using(() =>
+        //    {
+        //        var router = new RouterSocket();
+        //        router.Bind($"tcp://{Host}:{Port}");
+        //        return router; // "server"
+        //    },
+        //    router => source.Select(
+        //        message =>
+        //        {
+        //            var clientMessage = router.ReceiveMultipartMessage(); // This method doesn't really work. We only try and receive a message when our source emits. Actully we want to listen for clients and then send messages back to clients when we get a message. On the other hand, what would we send back if the server has nothing to emit?
+        //            uint clientAddress = (uint)clientMessage[0].ConvertToInt32();
+        //            var messagePayload = clientMessage[2].ToByteArray(); // Index as two as 2nd message entry is empty delimiter
+
+        //            var messageToClient = new NetMQMessage();
+        //            messageToClient.Append(clientMessage[0].ToByteArray());
+        //            messageToClient.AppendEmptyFrame();
+        //            messageToClient.Append(message.Buffer.Array);
+        //            router.SendMultipartMessage(messageToClient);
+
+        //            return new IncomingMessage(clientAddress, messagePayload);
+        //        })
+        //    );
+        //}
+
+        //public struct IncomingMessage
+        //{
+        //    public uint RoutingId;
+        //    public byte[] MessagePayload;
+
+        //    public IncomingMessage(uint routingId, byte[] messagePayload)
+        //    {
+        //        RoutingId = routingId;
+        //        MessagePayload = messagePayload;
+        //    }
+        //}
     }
 }
