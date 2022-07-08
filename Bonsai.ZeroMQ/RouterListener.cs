0b42ba1a0a3ea01821f78972924984a650c70137
RoboDoig <erskine94@gmail.com>
2022-07-08 15:28:45 +0100

    router sender must replay client messages to listen for both router socket and incoming message

--- Bonsai.ZeroMQ/RouterListener.cs
+++ Bonsai.ZeroMQ/RouterListener.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Bonsai.Osc;
--- Bonsai.ZeroMQ/RouterSender.cs
+++ Bonsai.ZeroMQ/RouterSender.cs
@@ -12,31 +12,22 @@ namespace Bonsai.ZeroMQ
     {
         public IObservable<Tuple<byte[], Message>> Process(IObservable<RouterSocket> source, IObservable<Tuple<byte[], Message>> clientMessage)
         {
-            return source.SelectMany(router =>
+            return clientMessage.Replay(replayMessage =>
             {
-                return clientMessage.Do(message =>
+                return source.SelectMany(router =>
                 {
-                    var messageToClient = new NetMQMessage();
-                    messageToClient.Append(message.Item1);
-                    messageToClient.AppendEmptyFrame();
-                    messageToClient.Append(message.Item2.Buffer.Array);
+                    return replayMessage.Do(message =>
+                    {
+                        var messageToClient = new NetMQMessage();
+                        //messageToClient.Append(message.Item1);
+                        messageToClient.Append(new byte[5]);
+                        messageToClient.AppendEmptyFrame();
+                        messageToClient.Append(message.Item2.Buffer.Array);
 
-                    router.SendMultipartMessage(messageToClient);
+                        router.SendMultipartMessage(messageToClient);
+                    });
                 });
             });
-            //return clientMessage.Do(m =>
-            //{
-            //    var router = source.TakeLast(1);
-            //    var messageToClient = new NetMQMessage();
-            //    messageToClient.Append(m.Item1);
-            //    messageToClient.AppendEmptyFrame();
-            //    messageToClient.Append(m.Item2.Buffer.Array);
-            //    router.Do(r =>
-            //    {
-            //        Console.WriteLine("Sending");
-            //        r.SendMultipartMessage(messageToClient);
-            //    });
-            //});
         }
     }
 }
