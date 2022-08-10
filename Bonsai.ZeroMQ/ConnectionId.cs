27f3bca61321ecd4b5dbb337bd7c06a9abcc58db
RoboDoig <erskine94@gmail.com>
2022-08-10 14:30:54 +0100

    Example docs, have push socket output ZeroMQMessage

--- Bonsai.ZeroMQ/ConnectionId.cs
+++ Bonsai.ZeroMQ/ConnectionId.cs
@@ -6,18 +6,43 @@ using System.Threading.Tasks;
 
 namespace Bonsai.ZeroMQ
 {
+    /// <summary>
+    /// Represents a connection identity for a ZeroMQ socket including the socket type, protocol and address.
+    /// </summary>
     public class ConnectionId
     {
+        /// <summary>
+        /// Gets or sets a value specifying the socket connection type (bind / connect).
+        /// </summary>
         public SocketSettings.SocketConnection SocketConnection { get; set; }
+        /// <summary>
+        /// Gets or sets a value specifying the network protocol of the socket connection.
+        /// </summary>
         public SocketSettings.SocketProtocol SocketProtocol {get; set;}
+        /// <summary>
+        /// Gets or sets a value specifying the host address of the socket connection.
+        /// </summary>
         public string Host { get; set; }
+        /// <summary>
+        /// Gets or sets a value specifying the port of the socket connection.
+        /// </summary>
         public string Port { get; set; }
 
+        /// <summary>
+        /// Parameterlesss constructor of the <see cref="ConnectionId"/> class for serialization.
+        /// </summary>
         public ConnectionId()
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionId"/> class. 
+        /// </summary>
+        /// <param name="socketType">The socket type (bind / connect).</param>
+        /// <param name="socketProtocol">The network protocol of the socket.</param>
+        /// <param name="host">The socket host address.</param>
+        /// <param name="port">The socket port.</param>
         public ConnectionId(SocketSettings.SocketConnection socketType, SocketSettings.SocketProtocol socketProtocol, string host, string port)
         {
             SocketConnection = socketType;
@@ -26,6 +51,10 @@ namespace Bonsai.ZeroMQ
             Port = port;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionId"/> class from an address string in the form "{socketType}{socketProtocol}://{host}:{port}", e.g. @tcp://localhost:5557.
+        /// </summary>
+        /// <param name="connectionString"></param>
         public ConnectionId(string connectionString)
         {
             string port = connectionString.Split(':')[2];
@@ -39,6 +68,7 @@ namespace Bonsai.ZeroMQ
             SocketConnection = SocketSettings.CharAsSocketConnection(type);
         }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             if (SocketConnection == SocketSettings.SocketConnection.Bind)
--- Bonsai.ZeroMQ/Push.cs
+++ Bonsai.ZeroMQ/Push.cs
@@ -7,23 +7,37 @@ using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
-    public class Push : Sink<Message>
+    /// <summary>
+    /// Represents an operator that creates a Push socket to send sequences of <see cref="Message"/>.
+    /// </summary>
+    public class Push : Combinator<Message, ZeroMQMessage>
     {
+        /// <summary>
+        /// Gets or sets a value specifying the <see cref="ZeroMQ.ConnectionId"/> of the <see cref="Push"/> socket.
+        /// </summary>
         [TypeConverter(typeof(ConnectionIdConverter))]
         public ConnectionId ConnectionId { get; set; } = new ConnectionId(SocketSettings.SocketConnection.Connect, SocketSettings.SocketProtocol.TCP, "localhost", "5557");
 
-        public override IObservable<Message> Process(IObservable<Message> source)
+        /// <summary>
+        /// Creates a Push socket with the specified <see cref="ZeroMQ.ConnectionId"/>.
+        /// </summary>
+        /// <param name="source">
+        /// A <see cref="Message"/> sequence to be sent by the socket.
+        /// </param>
+        /// <returns>
+        /// A <see cref="ZeroMQMessage"/> sequence representing messages sent by the socket.
+        /// </returns>
+        public override IObservable<ZeroMQMessage> Process(IObservable<Message> source)
         {
             return Observable.Using(() =>
             {
                 var push = new PushSocket(ConnectionId.ToString());
                 return push;
             },
-            push => source.Do(message => {
+            push => source.Select(message => {
                 push.TrySendFrame(message.Buffer.Array);
-            }).Finally(() => { push.Dispose(); })); 
+                return new ZeroMQMessage { Address = null, Message = message.Buffer.Array, MessageType = MessageType.Push };
+            }).Finally(() => { push.Dispose(); }));
         }
     }
-
-
 }
