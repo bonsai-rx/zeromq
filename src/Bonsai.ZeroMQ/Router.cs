7df467f0f19b8c747d3c253ba9ef4eb76ffb9d15
glopesdev <glopesdev@users.noreply.github.com>
2022-12-21 01:00:36 +0000

    Refactor router operator to follow req-rep pattern

--- src/Bonsai.ZeroMQ/Router.cs
+++ src/Bonsai.ZeroMQ/Router.cs
@@ -2,79 +2,101 @@
 using System.ComponentModel;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using Bonsai.Osc;
 using NetMQ;
 using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
     /// <summary>
-    /// Represents an operator that creates a Dealer socket to act as either client listener or both client listener and sender of sequence of <see cref="Message"/>.
+    /// Represents an operator that creates a router socket for tracking the identity of
+    /// received requests so that responses can be matched even if computed concurrently.
     /// </summary>
-    public class Router : Source<ZeroMQMessage>
+    public class Router : Source<ResponseContext>
     {
         /// <summary>
-        /// Gets or sets a value specifying the connection string of the <see cref="Router"/> socket.
+        /// Gets or sets a value specifying the endpoints to attach the socket to.
         /// </summary>
         [TypeConverter(typeof(ConnectionStringConverter))]
+        [Description("Specifies the endpoints to attach the socket to.")]
         public string ConnectionString { get; set; } = Constants.DefaultConnectionString;
 
         /// <summary>
-        /// If no <see cref="Message"/> sequence is provided as source, creates a Router socket that acts only as a client listener.
+        /// Generates an observable sequence of requests received from a router socket,
+        /// where the identity of each request is tracked so that responses can be matched
+        /// even if computed concurrently.
         /// </summary>
         /// <returns>
-        /// A sequence of <see cref="ZeroMQMessage"/> representing messages received by the socket.
+        /// An observable sequence of <see cref="ResponseContext"/> objects representing
+        /// received requests.
         /// </returns>
-        public override IObservable<ZeroMQMessage> Generate()
+        public override IObservable<ResponseContext> Generate()
         {
-            return Generate(null);
+            return Observable.Create<ResponseContext>((observer, cancellationToken) =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    var sendMutex = new object();
+                    using var router = new RouterSocket(ConnectionString);
+                    using var cancellation = cancellationToken.Register(router.Close);
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        var message = router.ReceiveMultipartMessage();
+                        var requestMessage = new ResponseContext(message);
+                        requestMessage.Response.Subscribe(response =>
+                        {
+                            lock (sendMutex)
+                            {
+                                router.SendMultipartMessage(response);
+                            }
+                        });
+                        observer.OnNext(requestMessage);
+                    }
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            });
         }
 
         /// <summary>
-        /// If a <see cref="Message"/> sequence is provided as source, creates a Router socket that acts as both a client listener and sender of <see cref="Message"/>.
+        /// Creates a router socket that appends an identity to all incoming messages
+        /// and reads it back from outgoing messages to determine the peer the message
+        /// should be routed to.
         /// </summary>
-        /// <param name="message">
-        /// A <see cref="Message"/> sequence to be sent by the socket.
+        /// <param name="source">
+        /// The sequence of <see cref="NetMQMessage"/> objects representing the
+        /// multiple part request messages to route back to peers.
         /// </param>
         /// <returns>
-        /// A sequence of <see cref="ZeroMQMessage"/> representing messages received by the socket.
+        /// An observable sequence of <see cref="NetMQMessage"/> objects representing
+        /// uncoming multiple part responses received from the router socket.
         /// </returns>
-        public IObservable<ZeroMQMessage> Generate(IObservable<Tuple<byte[], Message>> message)
+        public IObservable<NetMQMessage> Generate(IObservable<NetMQMessage> source)
         {
-            return Observable.Create<ZeroMQMessage>((observer, cancellationToken) =>
+            return Observable.Create<NetMQMessage>((observer, cancellationToken) =>
             {
-                var router = new RouterSocket(ConnectionString);
-                cancellationToken.Register(() => { router.Dispose(); });
-
-                if (message != null)
+                return Task.Factory.StartNew(() =>
                 {
-                    var sender = message.Do(m =>
+                    var sendMutex = new object();
+                    using var router = new RouterSocket(ConnectionString);
+                    using var cancellation = cancellationToken.Register(router.Close);
+                    using var subscription = source.Subscribe(response =>
                     {
-                        var messageToClient = new NetMQMessage();
-                        messageToClient.Append(m.Item1);
-                        messageToClient.AppendEmptyFrame();
-                        messageToClient.Append(m.Item2.Buffer.Array);
-                        router.SendMultipartMessage(messageToClient);
-                    }).Subscribe();
+                        lock (sendMutex)
+                        {
+                            router.SendMultipartMessage(response);
+                        }
+                    });
 
-                    cancellationToken.Register(() => sender.Dispose());
-                }
-
-                return Task.Factory.StartNew(() => {
                     while (!cancellationToken.IsCancellationRequested)
                     {
-                        var messageFromClient = router.ReceiveMultipartMessage();
-                        byte[] clientAddress = messageFromClient[0].ToByteArray();
-                        byte[] messagePayload = messageFromClient[2].ToByteArray();
-
-                        observer.OnNext(new ZeroMQMessage
-                        {
-                            Address = clientAddress,
-                            Message = messagePayload,
-                            MessageType = MessageType.Router
-                        });
+                        var message = router.ReceiveMultipartMessage();
+                        observer.OnNext(message);
                     }
-                });
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
             });
         }
     }
