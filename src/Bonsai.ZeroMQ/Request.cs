2575a3373025f8e7e6882d609f21d996d33bcb4a
glopesdev <glopesdev@users.noreply.github.com>
2022-12-21 00:56:16 +0000

    Refactor request operator to use only ZMQ types

--- src/Bonsai.ZeroMQ/Request.cs
+++ src/Bonsai.ZeroMQ/Request.cs
@@ -1,52 +1,87 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Reactive.Linq;
-using Bonsai.Osc;
+using System.Threading.Tasks;
 using NetMQ;
 using NetMQ.Sockets;
 
 namespace Bonsai.ZeroMQ
 {
     /// <summary>
-    /// Represents an operator that creates a Request socket to send sequences of <see cref="Message"/> and receive a response from a <see cref="Response"/>.
+    /// Represents an operator that creates request sockets for transmitting a sequence
+    /// of request messages and receiving responses as part of the req-rep pattern.
     /// </summary>
-    public class Request : Combinator<Message, ZeroMQMessage>
+    [Description("Creates request sockets for transmitting a sequence of request messages and receiving responses as part of the req-rep pattern.")]
+    public class Request : Combinator<byte[], NetMQMessage>
     {
         /// <summary>
-        /// Gets or sets a value specifying the connection string of the <see cref="Request"/> socket.
+        /// Gets or sets a value specifying the endpoints to attach the socket to.
         /// </summary>
         [TypeConverter(typeof(ConnectionStringConverter))]
+        [Description("Specifies the endpoints to attach the socket to.")]
         public string ConnectionString { get; set; } = Constants.DefaultConnectionString;
 
         /// <summary>
-        /// Creates a request socket with the specified <see cref="ConnectionString"/>
+        /// Creates request sockets for transmitting an observable sequence
+        /// of binary-coded request messages and returns all received responses.
         /// </summary>
         /// <param name="source">
-        /// A <see cref="Message"/> sequence to be sent by the socket.
+        /// The sequence of binary-coded request messages to transmit.
         /// </param>
         /// <returns>
-        /// A <see cref="ZeroMQMessage"/> sequence representing the messages sent by the socket.
+        /// An observable sequence of <see cref="NetMQMessage"/> objects representing
+        /// multiple part responses received from the request socket.
         /// </returns>
-        public override IObservable<ZeroMQMessage> Process(IObservable<Message> source)
+        public override IObservable<NetMQMessage> Process(IObservable<byte[]> source)
         {
-            return Observable.Using(() =>
+            return source.SelectMany(request => Task.Factory.StartNew(() =>
             {
-                var request = new RequestSocket(ConnectionString);
-                return request;
-            },
-            request => source.Select(
-                message =>
-                {
-                    request.SendFrame(message.Buffer.Array);
+                using var requestSocket = new RequestSocket(ConnectionString);
+                requestSocket.SendFrame(request);
+                return requestSocket.ReceiveMultipartMessage();
+            }));
+        }
 
-                    return new ZeroMQMessage
-                    {
-                        Address = null,
-                        Message = request.ReceiveFrameBytes(),
-                        MessageType = MessageType.Request
-                    };
-                }).Finally(() => { request.Dispose(); })
-            );;
+        /// <summary>
+        /// Creates request sockets for transmitting an observable sequence
+        /// of <see cref="string"/> request messages and returns all received responses.
+        /// </summary>
+        /// <param name="source">
+        /// The sequence of <see cref="string"/> request messages to transmit.
+        /// </param>
+        /// <returns>
+        /// An observable sequence of <see cref="NetMQMessage"/> objects representing
+        /// multiple part responses received from the request socket.
+        /// </returns>
+        public IObservable<NetMQMessage> Process(IObservable<string> source)
+        {
+            return source.SelectMany(request => Task.Factory.StartNew(() =>
+            {
+                using var requestSocket = new RequestSocket(ConnectionString);
+                requestSocket.SendFrame(request);
+                return requestSocket.ReceiveMultipartMessage();
+            }));
+        }
+
+        /// <summary>
+        /// Creates request sockets for transmitting an observable sequence
+        /// of multiple part request messages and returns all received responses.
+        /// </summary>
+        /// <param name="source">
+        /// The sequence of multiple part request messages to transmit.
+        /// </param>
+        /// <returns>
+        /// An observable sequence of <see cref="NetMQMessage"/> objects representing
+        /// multiple part responses received from the request socket.
+        /// </returns>
+        public IObservable<NetMQMessage> Process(IObservable<NetMQMessage> source)
+        {
+            return source.SelectMany(request => Task.Factory.StartNew(() =>
+            {
+                using var requestSocket = new RequestSocket(ConnectionString);
+                requestSocket.SendMultipartMessage(request);
+                return requestSocket.ReceiveMultipartMessage();
+            }));
         }
     }
 }
