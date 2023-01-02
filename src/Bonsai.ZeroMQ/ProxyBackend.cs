6a9f8d406c41b8a12481c5da543641866ff67bd6
glopesdev <glopesdev@users.noreply.github.com>
2023-01-02 12:10:37 +0000

    Add support for creating and configuring proxies

---
+++ src/Bonsai.ZeroMQ/ProxyBackend.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using NetMQ;
+
+namespace Bonsai.ZeroMQ
+{
+    /// <summary>
+    /// Represents an operator that creates and starts a proxy with the specified
+    /// back-end socket.
+    /// </summary>
+    [Description("Creates and starts a proxy with the specified back-end socket.")]
+    public class ProxyBackend : Combinator<SocketInfo, Unit>, INamedElement
+    {
+        /// <summary>
+        /// Gets or sets a value specifying the endpoints to attach the socket to.
+        /// </summary>
+        [TypeConverter(typeof(ConnectionStringConverter))]
+        [Description("Specifies the endpoints to attach the socket to.")]
+        public string ConnectionString { get; set; } = Constants.DefaultConnectionString;
+
+        /// <summary>
+        /// Gets or sets a value specifying the type of socket to use as the back-end
+        /// for a proxy.
+        /// </summary>
+        [Description("Specifies the type of socket to use as the back-end.")]
+        public SocketType SocketType { get; set; } = SocketType.XPublisher;
+
+        string INamedElement.Name => $"{SocketType}";
+
+        /// <summary>
+        /// Creates and starts a proxy with the specified back-end socket.
+        /// </summary>
+        /// <param name="source">
+        /// A sequence of <see cref="SocketInfo"/> objects representing information
+        /// about the front-end socket to use for the proxy. A new proxy will be
+        /// created and started for each value in the sequence.
+        /// </param>
+        /// <returns>
+        /// An observable sequence whose observers will never get called.
+        /// The proxy is started purely for its side-effects of routing messages
+        /// from the front-end to the back-end sockets.
+        /// </returns>
+        public override IObservable<Unit> Process(IObservable<SocketInfo> source)
+        {
+            return source.SelectMany(frontendInfo => Observable.Create<Unit>((observer, cancellationToken) =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    using var frontend = SocketFactory.Create(frontendInfo.SocketType, frontendInfo.ConnectionString);
+                    using var backend = SocketFactory.Create(SocketType, ConnectionString);
+                    var proxy = new Proxy(frontend, backend);
+                    using var cancellation = cancellationToken.Register(proxy.Stop);
+                    proxy.Start();
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            }));
+        }
+    }
+}
---
+++ src/Bonsai.ZeroMQ/SocketInfo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Bonsai.ZeroMQ
+{
+    /// <summary>
+    /// Represents information required for creating a <see cref="NetMQSocket"/>.
+    /// </summary>
+    public class SocketInfo
+    {
+        internal SocketInfo(SocketType socketType, string connectionString = null)
+        {
+            SocketType = socketType;
+            ConnectionString = connectionString;
+        }
+
+        internal SocketType SocketType { get; }
+
+        internal string ConnectionString { get; }
+    }
+
+    /// <summary>
+    /// Specifies the basic types of message-queue sockets implementing the
+    /// core patterns (pub-sub, req-rep, dealer-router, etc).
+    /// </summary>
+    public enum SocketType
+    {
+        /// <summary>
+        /// Specifies a <see cref="PairSocket"/>.
+        /// </summary>
+        Pair = ZmqSocketType.Pair,
+
+        /// <summary>
+        /// Specifies a <see cref="PublisherSocket"/>.
+        /// </summary>
+        Publisher = ZmqSocketType.Pub,
+
+        /// <summary>
+        /// Specifies a <see cref="SubscriberSocket"/>.
+        /// </summary>
+        Subscriber = ZmqSocketType.Sub,
+
+        /// <summary>
+        /// Specifies a <see cref="RequestSocket"/>.
+        /// </summary>
+        Request = ZmqSocketType.Req,
+
+        /// <summary>
+        /// Specifies a <see cref="ResponseSocket"/>.
+        /// </summary>
+        Response = ZmqSocketType.Rep,
+
+        /// <summary>
+        /// Specifies a <see cref="DealerSocket"/>.
+        /// </summary>
+        Dealer = ZmqSocketType.Dealer,
+
+        /// <summary>
+        /// Specifies a <see cref="RouterSocket"/>.
+        /// </summary>
+        Router = ZmqSocketType.Router,
+
+        /// <summary>
+        /// Specifies a <see cref="PullSocket"/>.
+        /// </summary>
+        Pull = ZmqSocketType.Pull,
+
+        /// <summary>
+        /// Specifies a <see cref="PushSocket"/>.
+        /// </summary>
+        Push = ZmqSocketType.Push,
+
+        /// <summary>
+        /// Specifies a <see cref="XPublisherSocket"/>.
+        /// </summary>
+        XPublisher = ZmqSocketType.Xpub,
+
+        /// <summary>
+        /// Specifies a <see cref="XSubscriberSocket"/>.
+        /// </summary>
+        XSubscriber = ZmqSocketType.Xsub,
+
+        /// <summary>
+        /// Specifies a <see cref="StreamSocket"/>.
+        /// </summary>
+        Stream = ZmqSocketType.Stream
+    }
+
+    internal static class SocketFactory
+    {
+        internal static NetMQSocket Create(SocketType socketType, string connectionString = null)
+        {
+            return socketType switch
+            {
+                SocketType.Pair => new PairSocket(connectionString),
+                SocketType.Publisher => new PublisherSocket(connectionString),
+                SocketType.Subscriber => new SubscriberSocket(connectionString),
+                SocketType.Request => new RequestSocket(connectionString),
+                SocketType.Response => new ResponseSocket(connectionString),
+                SocketType.Dealer => new DealerSocket(connectionString),
+                SocketType.Router => new RouterSocket(connectionString),
+                SocketType.Pull => new PullSocket(connectionString),
+                SocketType.Push => new PushSocket(connectionString),
+                SocketType.XPublisher => new XPublisherSocket(connectionString),
+                SocketType.XSubscriber => new XSubscriberSocket(connectionString),
+                SocketType.Stream => new StreamSocket(connectionString),
+                _ => throw new NotSupportedException("Unsupported socket type.")
+            };
+        }
+    }
+}
