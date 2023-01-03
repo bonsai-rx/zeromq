645d92e844b036292087e37e6a1b793f20fcd8af
glopesdev <glopesdev@users.noreply.github.com>
2023-01-03 12:42:30 +0000

    Allow automatically resolving response identities

--- src/Bonsai.ZeroMQ/SendResponse.cs
+++ src/Bonsai.ZeroMQ/SendResponse.cs
@@ -56,9 +56,12 @@ namespace Bonsai.ZeroMQ
             {
                 var selector = Expression.Lambda(selectorBody, inputParameter);
                 var selectorObservableType = selector.ReturnType.GetGenericArguments()[0];
-                if (selectorObservableType != typeof(NetMQMessage))
+                if (selectorObservableType != typeof(NetMQMessage) &&
+                    selectorObservableType != typeof(NetMQFrame) &&
+                    selectorObservableType != typeof(string) &&
+                    selectorObservableType != typeof(byte[]))
                 {
-                    throw new InvalidOperationException($"The specified response workflow must have a single output of type {typeof(NetMQMessage)}.");
+                    throw new InvalidOperationException($"The type of the response workflow is not compatible with {typeof(NetMQMessage)}.");
                 }
 
                 return Expression.Call(GetType(), nameof(Process), null, source, selector);
@@ -70,6 +73,65 @@ namespace Bonsai.ZeroMQ
             return source.SelectMany(context => selector(Observable
                 .Return(context.Request))
                 .LastAsync()
+                .Do(context.Response));
+        }
+
+        static IObservable<NetMQMessage> Process(
+            IObservable<ResponseContext> source,
+            Func<IObservable<NetMQMessage>, IObservable<byte[]>> selector)
+        {
+            return Process(source, selector, (response, buffer) => response.Append(buffer));
+        }
+
+        static IObservable<NetMQMessage> Process(
+            IObservable<ResponseContext> source,
+            Func<IObservable<NetMQMessage>, IObservable<string>> selector)
+        {
+            return Process(source, selector, (response, message) => response.Append(message, SendReceiveConstants.DefaultEncoding));
+        }
+
+        static IObservable<NetMQMessage> Process(
+            IObservable<ResponseContext> source,
+            Func<IObservable<NetMQMessage>, IObservable<NetMQFrame>> selector)
+        {
+            return Process(source, selector, (response, frame) => response.Append(frame));
+        }
+
+        static IObservable<NetMQMessage> Process<TFrame>(
+            IObservable<ResponseContext> source,
+            Func<IObservable<NetMQMessage>, IObservable<TFrame>> selector,
+            Action<NetMQMessage, TFrame> appendFrame)
+        {
+            return source.SelectMany(context => selector(Observable
+                .Return(context.Request))
+                .ToList()
+                .Select(frames =>
+                {
+                    var delimiter = context.Request.FrameCount - 1;
+                    for (; delimiter >= 0; delimiter--)
+                    {
+                        if (context.Request[delimiter].IsEmpty)
+                        {
+                            break;
+                        }
+                    }
+
+                    var response = new NetMQMessage(frames.Count + delimiter + 1);
+                    if (delimiter >= 0)
+                    {
+                        for (int i = 0; i <= delimiter; i++)
+                        {
+                            response.Append(context.Request[i]);
+                        }
+                    }
+
+                    foreach (var frame in frames)
+                    {
+                        appendFrame(response, frame);
+                    }
+
+                    return response;
+                })
                 .Do(context.Response));
         }
     }
