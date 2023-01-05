d70558447c029376388d620f1741a5ae1c7a0c78
RoboDoig <erskine94@gmail.com>
2023-01-05 15:01:20 +0000

    Documentation for ZyreNode and message converters

--- src/Bonsai.ZeroMQ/ToZyreShout.cs
+++ src/Bonsai.ZeroMQ/ToZyreShout.cs
@@ -8,9 +8,21 @@ using System.Threading.Tasks;
 
 namespace Bonsai.ZeroMQ
 {
-    public class ToZyreShout : Transform<Tuple<NetMQMessage, string>, ZyreMessage>
+    /// <summary>
+    /// Represents an operator that packages a <see cref="NetMQMessage"/> as a Zyre shout message.
+    /// </summary>
+    public class ToZyreShout : Transform<Tuple<NetMQMessage, string>, ZyreMessageShout>
     {
-        public override IObservable<ZyreMessage> Process(IObservable<Tuple<NetMQMessage, string>> source)
+        /// <summary>
+        /// Transforms and observable sequence of messages and target groups to an observable sequence of Zyre shout messages.
+        /// </summary>
+        /// <param name="source">
+        /// A union of <see cref="NetMQMessage"/> containing the message data and a <see cref="string"/> referencing the target group of the shout.
+        /// </param>
+        /// <returns>
+        /// An observable sequence of <see cref="ZyreMessageShout"/>.
+        /// </returns>
+        public override IObservable<ZyreMessageShout> Process(IObservable<Tuple<NetMQMessage, string>> source)
         {
             return source.Select(x => new ZyreMessageShout { CommandType = ZyreCommandType.Shout, Group = x.Item2, Message = x.Item1 });
         }
