909278413d0c59351ed753cc38d642b989d8fa07
Shawn Tan <banchan@gmail.com>
2024-04-14 22:56:54 +0800

    updated docs to latest infrastructure and theme

---
+++ docs/build.ps1
@@ -0,0 +1,16 @@
+.\bonsai\modules\Export-Image.ps1 "..\src\Bonsai.ZeroMQ\bin\Release\net472"
+$exampleEnvironments = Get-ChildItem -Directory -Recurse -Filter ".bonsai" -Path "examples"
+foreach ($environment in $exampleEnvironments)
+{
+    # bootstrap environment
+    $galleryPath = Join-Path $environment.FullName "Gallery"
+    New-Item -Path $galleryPath -Force -ItemType Directory | Out-Null
+    Get-ChildItem -Path "..\src\bin\Release" -Filter *.nupkg | Copy-Item -Destination $galleryPath
+    . (Join-Path $environment.FullName "Setup.ps1")
+
+    # export example workflows
+    $workflowPath = $environment.Parent.FullName
+    $bootstrapperPath = Join-Path $environment.FullName "Bonsai.exe"
+    .\bonsai\modules\Export-Image.ps1 -workflowPath $workflowPath -bootstrapperPath $bootstrapperPath
+}
+dotnet docfx @args
